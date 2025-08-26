017c79c2a8928f3689d8b623625b014a71b90a03
Gryghor camonni ferreira cesar <gcessar@saude.inet>
2025-08-26 14:05:47 -0300

    Adição da página de Dados Gerais e seus componentes

---
+++ front-vigiasus/src/components/dados-gerais/gerenciaCard.tsx
@@ -0,0 +1,25 @@
+// components/dados-gerais/GerenciaCard.tsx
+
+interface GerenciaCardProps {
+  label: string;
+  color: string;
+}
+
+export default function GerenciaCard({ label, color }: GerenciaCardProps) {
+  return (
+    // MUDANÇA 1: Adicionadas as classes 'w-full' e 'h-full'
+    // para forçar o card a ocupar todo o espaço do seu container pai.
+    <div className="flex h-full w-full items-center overflow-hidden rounded-lg bg-white shadow-md">
+      <div
+        className="w-3 self-stretch"
+        style={{ backgroundColor: color }}
+      ></div>
+
+      {/* Truncate serve para limitar o tamanho do texto "..."
+      */}
+      <span className="truncate py-3 px-4 m-2 font-medium text-gray-800">
+        {label}
+      </span>
+    </div>
+  );
+}
