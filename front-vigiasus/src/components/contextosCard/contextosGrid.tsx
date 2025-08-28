9044821da7ebc8699afcfb723173e0ea374131d9
pedro <sakki.backup@gmail.com>
2025-08-28 11:42:17 -0300

    agora já foi

--- front-vigiasus/src/components/contextosCard/contextosGrid.tsx
+++ front-vigiasus/src/components/contextosCard/contextosGrid.tsx
@@ -17,18 +17,23 @@ interface FileGridProps {
 
 export function FileGrid({ files, onFileClick, className }: FileGridProps) {
     return (
-        <div className="flex1 items-center max-w-[82%]">
-            <div className={`grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-x-1 mb-8 mx-auto ${className}`}>
-                {files.map((file) => (
-                    <FileItem
-                        key={file.id}
-                        title={file.title}
-                        type={file.type}
-                        insertedDate={file.insertedDate}
-                        onClick={() => onFileClick?.(file)}
-                        className="w-full"
-                    />
-                ))}
+        <div className="flex justify-center items-center w-full h-full">
+            <div className="bg-white border border-[#f0f0f0] rounded-4xl shadow-lg backdrop-blur-sm flex flex-col items-center justify-center p-6">
+                <div
+                    className={`grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-x-6 mx-auto custom-scroll`}
+                    style={{ maxHeight: '520px', overflowY: 'auto' }}
+                >
+                    {files.map((file) => (
+                        <FileItem
+                            key={file.id}
+                            title={file.title}
+                            type={file.type}
+                            insertedDate={file.insertedDate}
+                            onClick={() => onFileClick?.(file)}
+                            className="w-full"
+                        />
+                    ))}
+                </div>
             </div>
         </div>
 
