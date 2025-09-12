b3d2ea01cf357e22542e4c98e7e34b4d9e75f321
Gryghor camonni ferreira cesar <gcessar@saude.inet>
2025-09-12 10:32:41 -0300

    adicionando caledário interativo

--- front-vigiasus/src/components/conselho/eventSection.tsx
+++ front-vigiasus/src/components/conselho/eventSection.tsx
@@ -2,13 +2,14 @@
 
 "use client";
 
-import React from 'react';
+import React, {useState} from 'react';
 import { Plus } from 'lucide-react';
 import { events } from '@/constants/eventsData';
 import EventCard from '@/components/conselho/eventCard';
+import AddEventModal from '../popups/addEventModal';
 
   export default function EventsSection() {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+ const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <section className="bg-white py-12 px-6">
@@ -29,17 +30,14 @@ import EventCard from '@/components/conselho/eventCard';
         ))}
       </div>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h3 className="text-xl font-bold">Adicionar Novo Evento</h3>
-            <p className="mt-2 text-sm text-gray-600">Formulário de evento aqui...</p>
-            <button onClick={() => setIsModalOpen(false)} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
-              Fechar
-            </button>
-          </div>
-        </div>
-      )}
+    <AddEventModal
+  isOpen={isModalOpen}
+  onClose={() => setIsModalOpen(false)}
+  onSave={(event) => {
+    console.log("Novo evento:", event);
+    // aqui você pode salvar no estado, backend, etc
+  }}
+/>
     </section>
   );
 }
